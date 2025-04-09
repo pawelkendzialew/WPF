ba0a7744bd661318a692becb2a8a646d8e20d0ae
Lew <148969744+pawelkendzialew@users.noreply.github.com>
2025-04-10 00:15:57 +0200

    g
    g

---
+++ WpfApp3/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        Towar t1 = new Towar();
+        t1.Ilosc = 10;
+        t1.Cena = 5.0;
+
+        Towar t2 = new Towar();
+        t2.Ilosc = 20;
+        t2.Cena = 7.0;
+
+        Towar t3 = t1 + t2;
+
+        MessageBox.Show($"Towar 1: {t1}\nTowar 2: {t2}\n\nPo dodaniu: {t3}");
+    }
+}
---
+++ WpfApp3/WpfApp3/Towar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    class Towar
+    {
+        public int Ilosc;
+        public double Cena;
+
+        public override string ToString()
+        {
+            return $"Ilość: {Ilosc}, Cena: {Cena:F2}";
+        }
+
+        public static Towar operator +(Towar t1, Towar t2)
+        {
+            Towar wynik = new Towar();
+            wynik.Ilosc = t1.Ilosc + t2.Ilosc;
+            wynik.Cena = (t1.Cena + t2.Cena) / 2.0;
+            return wynik;
+        }
+    }
+}
