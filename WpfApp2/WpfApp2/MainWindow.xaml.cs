ff5f39357c1d3ba643f10eead07554d92f5f685b
Lew <148969744+pawelkendzialew@users.noreply.github.com>
2025-04-10 00:17:07 +0200

    g
    g

---
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+        double[] liczby = new double[] { 1.1, 2.2, 3.3, 4.4, 5.5 };
+
+        Window1 okno = new Window1();
+        okno.Liczby = liczby; // właściwość tylko do zapisu
+        okno.Show();
+    }
+
+}
---
+++ WpfApp2/WpfApp2/Window1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        public double[] Liczby
+        {
+            set
+            {
+                listBox.Items.Clear();
+                for (int i = 0; i < value.Length; i++)
+                {
+                    listBox.Items.Add(value[i]);
+                }
+            }
+        }
+    }
+}
